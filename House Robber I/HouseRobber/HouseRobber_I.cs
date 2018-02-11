74725def01a6df2a4eadf81f1b163ebc68ff93d0
marco-dorantes <marcod@gmail.com>
2018-02-11 16:53:15 -0600

    Added House Robber I

---
+++ House Robber I/HouseRobber/HouseRobber_I.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HouseRobber
+{
+  /// <summary>
+  /// You are a professional robber planning to rob houses along a street.
+  /// Each house has a certain amount of money stashed, the only constraint stopping you from robbing
+  /// each of them is that adjacent houses have security system connected and it will automatically
+  /// contact the police if two adjacent houses were broken into on the same night.
+  ///
+  /// Given a list of non-negative integers representing the amount of money of each house, determine
+  /// the maximum amount of money you can rob tonight without alerting the police.
+  ///
+  /// The restriction is that you can't use two consecutive numbers (in an array) in a row.
+  /// You can jump like this, 1, 3, 5, 7 or 2, 4, 6, 8. And you could also skip more than 1 number.
+  /// You might do a one-pass scan from left to right, and compute the maximum values to such position.
+  ///
+  /// Please design two different algorithms to solve the same problem from two different design perspectives.
+  /// Also, please ask for all the help you need along the way of your analysis and design efforts.
+  /// </summary>
+  public static class HouseRobber_I
+    {
+    public static int Algorithm1(int[] house) => throw new NotImplementedException();
+    public static int Algorithm2(int[] house) => throw new NotImplementedException();
+  }
+}
---
+++ House Robber I/Specification/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Specification")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Specification")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f514627-ff6d-4b3a-a681-2dd9406c1cb7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
